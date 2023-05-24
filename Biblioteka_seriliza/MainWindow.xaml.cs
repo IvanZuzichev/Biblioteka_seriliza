8d71c70dee12304ebfcb42113361ff153bbb8371
IvanZuzichev <isip_i.s.zuzichev@mpt.ru>
2023-05-24 12:56:24 +0300

    Добавьте файлы проекта.

---
+++ Biblioteka_seriliza/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Windows;
+using SerilazaLib;
+using System;
+
+namespace Biblioteka_seriliza
+{
+    public partial class MainWindow : Window
+    {
+         Class1 class1 = new Class1();
+        public MainWindow()
+        {
+            var s =  class1.DeSerilaz();
+            InitializeComponent();
+            vigruzka.Text = s;
+        }
+
+        private void serilizator_Click(object sender, RoutedEventArgs e)
+        {
+            string txt= vvod.Text;
+            class1.Serilaz(txt);
+            Close();
+        }
+    }
+}
---
+++ SerilazaLib/Class1.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Newtonsoft.Json;
+
+namespace SerilazaLib
+{
+    public class Class1
+    {
+        public void Serilaz(string text)
+        {
+            string json = JsonConvert.SerializeObject(text);
+            File.WriteAllText("D:\\JsonPract.json", json);
+            
+        } 
+        public string DeSerilaz()
+        {
+            string text = File.ReadAllText("D:\\JsonPract.json");
+            string jsontext = JsonConvert.DeserializeObject<string>(text);
+            return jsontext;
+            
+        }
+
+    }
+}
